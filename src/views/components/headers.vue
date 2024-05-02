ba4e025581e908c2c69d4d556bd9b66028a85c0b
Yang <yzr@yserver.top>
2024-05-02 23:58:05 +0800

    重构

--- src/views/components/headers.vue
+++ src/views/components/headers.vue
@@ -1,10 +1,12 @@
 <template>
 
   <div class="relative mb-4 diagonal-gradient">
-    <div class="flex pt-20px pl-20px">
+    <div class="flex justify-between  pt-5 pl-5">
+      <div class="flex">
       <div @click="openMenu">
         <svg t="1714319497904" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="5219" width="35" height="40"><path d="M212 338c-24.852 0-45-20.148-45-45S187.148 248 212 248h600c24.852 0 45 20.148 45 45S836.852 338 812 338H212z m0 220c-24.852 0-45-20.148-45-45S187.148 468 212 468h600c24.852 0 45 20.148 45 45S836.852 558 812 558H212z m0 220c-24.852 0-45-20.148-45-45S187.148 688 212 688h600c24.852 0 45 20.148 45 45S836.852 778 812 778H212z" fill="#ffffff" p-id="5220"></path></svg>
       </div>
+
       <v-mini-weather class="w-64 pl-20px pt-10px ">
         <template #default="{weather, icon}">
           <div class="flex text-amber-500 text-base" >
@@ -15,6 +17,17 @@
           </div>
         </template>
       </v-mini-weather>
+      </div>
+      <div class="mr-10px">
+        <n-dropdown trigger="hover" :options="menuOptions" @select="handleSelect">
+          <n-avatar round size="medium" :style="{ backgroundColor: randomColor, display: 'flex', }">
+            <n-icon size="20">
+              <Person />
+            </n-icon>
+          </n-avatar>
+
+        </n-dropdown>
+      </div>
     </div>
     <div class="s-search">
       <div id="search" class="s-search mx-auto mt-30px">
@@ -53,14 +66,109 @@
 </template>
 <script setup>
 import { vMiniWeather, vMiniWeatherIcon } from 'vue3-mini-weather'
-import {defineProps, ref} from 'vue'
+import {computed, defineProps, h, ref} from 'vue'
+import axios from "axios";
+import {NAvatar, NDropdown, NIcon, NText} from "naive-ui";
+import {
+  LogOutOutline as LogoutIcon,
+  Person,
+  PersonCircleOutline as UserIcon,
+  SettingsOutline as AdminIcon
+} from "@vicons/ionicons5";
+
 const props = defineProps({
   collapsed: { type: Boolean, required: true }
 })
 const emit = defineEmits(['update:collapsed']);
-
+const renderIcon = (icon) => {
+  return () => {
+    return h(NIcon, null, {
+      default: () => h(icon)
+    });
+  };
+};
 const openMenu = () => {
   emit('update:collapsed', !props.collapsed);
+}
+const systemInfo =ref(null)
+const userInfo=ref(null)
+const Logout = async () => {
+  try {
+    await axios.post('/api/logout');
+    localStorage.removeItem('user');
+    window.location.reload();
+    message.success('退出登录成功');
+  } catch (error) {
+    const msg = error.response?.data?.msg || '请检查网络连接';
+    message.error(msg);
+  }
+};
+const renderCustomHeader = () => {
+  return h(
+      "div",
+      {
+        style: "display: flex; align-items: center; padding: 8px 12px;"
+      },
+      [
+        h(NAvatar, {
+          round: true,
+          style: { 'margin-right': '12px', 'display': 'flex' },
+        },() => [h(NText, { depth: 1 }, { default: () => userInfo.value.username })]),
+        h("div", null, [
+          h("div", null, [h(NText, { depth: 2 }, { default: () => userInfo.value.nickname })]),
+          h("div", { style: "font-size: 12px;" }, [
+            h(
+                NText,
+                { depth: 3 },
+                { default: () => "欢迎访问Yserver服务平台" }
+            )
+          ])
+        ])
+      ]
+
+  );
+  //加入分割线
+
+}
+
+const menuOptions = computed(() => {
+  return [
+    {
+      key: "header",
+      type: "render",
+      render: renderCustomHeader
+    },
+    {
+      key: 'header-divider',
+      type: 'divider'
+    },
+    {
+      label: '用户信息',
+      key: 'user/record',
+      icon: renderIcon(UserIcon)
+    },
+    {
+      label: '后台管理',
+      key: 'admin',
+      icon: renderIcon(AdminIcon)
+
+    },
+    {
+      label: '退出登录',
+      key: 'logout',
+      icon: renderIcon(LogoutIcon)
+    },
+  ];
+});
+const handleSelect =(key)=>{
+  if (key==='logout'){
+    Logout()
+  }
+  else {
+    location.href = systemInfo.value.BASE_URL+'/'+key
+  }
+
+
 }
 const searchBar = ref([
   {
@@ -116,7 +224,19 @@ const search = (e) => {
   const searchText = document.getElementById('search-text').value
   window.open(`https://www.bing.com/search?q=${searchText}`)
 }
-
+const getUserInfo =async () => {
+  axios.get('/api/user/me', {withCredentials: true}).then(response => {
+    userInfo.value = response.data.result
+  }).catch(error => {
+    console.error('获取用户信息失败:', error);
+  });
+  try {
+    const response = await axios.get('/api/system/info');
+    systemInfo.value = response.data.result;
+  } catch (error) {
+  }
+}
+getUserInfo()
 </script>
 <style>
 .labels {
--- vite.config.js
+++ vite.config.js
@@ -21,5 +21,14 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:5000', // 后端服务地址
+        changeOrigin: true, // 需要虚拟托管站点，以便可以跨域
+        rewrite: (path) => path.replace(/^\/api/, ''), // 重写路径，移除 /api 前缀
+      },
+    },
+  },
 })
